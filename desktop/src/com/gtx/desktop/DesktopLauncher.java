3f3f07d223f280782e803901dcbf08222d9ab6a0
aThorp96 <ahthorp96@icloud.com>
2018-04-06 19:16:19 -0400

    initialized repo

---
+++ desktop/src/com/gtx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gtx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gtx.Rogue-Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Rogue-Game(), config);
+	}
+}
